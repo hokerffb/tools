48c818ca78eb28ac8026b3ca117bc34b5b4a6959
Ubuntu Precise <ubuntu@i-x0x2o0l1.(none)>
2015-05-05 15:45:53 +0800

    auto create default db

--- rds/run.sh
+++ rds/run.sh
@@ -4,6 +4,8 @@
 # MYSQLPASS="1a2b3c4d"
 # MYSQLDB="bench"
 
+mysql -hMYSQLHOST -PMYSQLPORT -uMYSQLUSER -pMYSQLPASS -e "create database $MYSQLDB;"
+
 sysbench --num-threads=16 --test=oltp --mysql-table-engine=innodb --mysql-host=$MYSQLHOST --mysql-port=$MYSQLPORT --mysql-db=$MYSQLDB --oltp-table-size=2000000 --max-time=1200 --mysql-user=$MYSQLUSER --mysql-password=$MYSQLPASS prepare
 
 sysbench --num-threads=16 --test=oltp --mysql-table-engine=innodb --mysql-host=$MYSQLHOST --mysql-port=$MYSQLPORT --mysql-db=$MYSQLDB --oltp-table-size=2000000 --max-time=1200 --mysql-user=$MYSQLUSER --mysql-password=$MYSQLPASS --max-time=120 run
