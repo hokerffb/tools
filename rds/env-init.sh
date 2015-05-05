cca3854134fbea9071e59f97267add43b81faa3f
0xff <pengyuwei@gmail.com>
2015-05-05 15:53:32 +0800

    add note text

--- rds/env-init.sh
+++ rds/env-init.sh
@@ -1,2 +1,10 @@
+##########################################
+#
+# 1. create a database user: testuser
+# 2. create database: bench
+# 3. execute 'sh run.sh' script
+#
+###########################################
+ 
 apt-get install -y git vim
 apt-get install -y mysql-client-core-5.5
