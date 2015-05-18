81dea57a323ff215b65183dbcfe5152a1b4008af
0xff <pengyuwei@gmail.com>
2015-05-18 17:04:36 +0800

    add insert test

--- rds/const.sh
+++ rds/const.sh
@@ -3,4 +3,5 @@ export MYSQLPORT="3306"
 export MYSQLUSER="testuser"
 export MYSQLPASS=""
 export MYSQLDB="bench"
+export MYSQLDBINS="ins-test"
 
--- rds/env-init.sh
+++ rds/env-init.sh
@@ -7,4 +7,10 @@
 ###########################################
  
 apt-get install -y git vim
-apt-get install -y mysql-client-core-5.5
+apt-get install -y mysql-client-core-5.5 python-mysqldb
+
+# init tables
+# must create database first 
+SQL="CREATE TABLE  `ins-test`.`ins` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,`value` VARCHAR( 1024 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL) ENGINE = MYISAM ;"
+mysql -h$MYSQLHOST -P$MYSQLPORT -u$MYSQLUSER -p$MYSQLPASS -e "$SQL" 
+
