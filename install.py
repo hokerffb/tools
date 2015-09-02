682813e30d7db5aa647310c4bc3d53410a02a394
0xff <pengyuwei@gmail.com>
2015-09-02 09:53:40 +0000

    add fdp tool

--- install.py
+++ install.py
@@ -4,5 +4,8 @@ chmod +x /usr/bin/where
 cp ./ref.py /usr/bin/ref
 chmod +x /usr/bin/ref
 
+cp ./fdp.py /usr/bin/fdp
+chmod +x /usr/bin/fdp
+
 echo 'where <python-moudle>'
 echo 'grep -rn "keyword" *|ref 5 5'
