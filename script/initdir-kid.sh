6d313540a5a34282b689fd69daa914e5ef3a7985
0xff <pengyuwei@gmail.com>
2021-03-02 16:08:05 +0800

    add script

---
+++ script/initdir-kid.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+echo '创建孩子的文档管理目录树'
+
+mkdir -p 课本作业
+mkdir -p 课本作业/三年级
+
+mkdir -p 画画
+mkdir -p 画画/三年级
+mkdir -p 画画/pyw协助
+
+mkdir -p 头像
+mkdir -p 证书
+mkdir -p 字帖
+mkdir -p 收到的礼物
+mkdir -p 合影
+mkdir -p 照片
+mkdir -p 游戏
+mkdir -p 评论
+mkdir -p 观测记录
+mkdir -p project
+
+mkdir -p 同学照片
+
+mkdir -p 日记
+mkdir -p 日记/2021
+
+mkdir -p 录音
+mkdir -p 录音/清唱
+mkdir -p 录音/配乐
+mkdir -p 录音/聊天录音
+
+mkdir -p 音乐
+mkdir -p 音乐/作曲和弹奏
+mkdir -p 音乐/音乐课
+mkdir -p 音乐/混音
+
+mkdir -p 医疗记录
+mkdir -p 医疗记录/疫苗相关
+mkdir -p 医疗记录/疫苗本
+mkdir -p 医疗记录/2021
+
+ls -R
---
+++ script/initenv-ubuntu.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt upgrade
+sudo apt install -y vim ssh net-tools
+sudo apt install -y build-essential mysql-server meld cargo
