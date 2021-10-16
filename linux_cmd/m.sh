084212ff05bb18cdb5bb02899527764bb0f069f4
0xff <pengyuwei@gmail.com>
2021-10-16 18:12:44 +0800

    update

--- linux_cmd/m.sh
+++ linux_cmd/m.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-ps aux --sort=-rss|head -20|awk '{printf ("%.2fMB %s %d\n",$6/1024,$11, $2)}'
+ps aux --sort=-rss|head -20|awk '{if(NR>1) printf ("%.2fMB %s %d\n",$6/1024,$11, $2)}'
 free -m
