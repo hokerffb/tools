899d0ceff83fcceada9f8fc8d8d82f67d4d85b3b
0xff <pengyuwei@gmail.com>
2023-04-14 21:25:09 +0800

    add tree

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y vim build-essential grc git net-tools tmux
+sudo apt install -y vim build-essential grc git net-tools tmux tree
 wget http://www.memcd.com/conf/vimrc.local
 sudo cp vimrc.local /etc/vim/
 echo "set -g mouse on" > ~/.tmux.conf 
