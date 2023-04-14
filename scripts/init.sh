1ecd4760ef6825c61baca1807ffa1a2954ccbe34
0xff <pengyuwei@gmail.com>
2023-04-14 21:16:12 +0800

    update

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt install -y vim build-essential grc git net-tools
+sudo apt install -y vim build-essential grc git net-tools tmux
 wget http://www.memcd.com/conf/vimrc.local
 sudo cp vimrc.local /etc/vim/
+echo "set -g mouse on" > ~/.tmux.conf 
 
