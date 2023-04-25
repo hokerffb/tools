59a2a281329ed1fb153be0c4f40848002d6870fd
0xff <pengyuwei@gmail.com>
2023-04-25 15:28:31 +0800

    init ubuntu22.04 base env

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-sudo apt install -y vim build-essential grc git net-tools tmux tree
+wget http://www.memcd.com/conf/ubuntu22.04/source.list.ustc
+sudo cp source.list.ustc /etc/apt/sources.list
+sudo apt update
+
+sudo apt install -y vim build-essential grc git wget net-tools tmux tree
 wget http://www.memcd.com/conf/vimrc.local
 sudo cp vimrc.local /etc/vim/
 echo "set -g mouse on" > ~/.tmux.conf 
+
+wget http://www.memcd.com/conf/docker/daemon.json
+sudo cp daemon.json /etc/docker/
+
+wget http://www.memcd.com/conf/.bash_aliases -O ~/.bash_aliases
+source ~/.bash_aliases
+
+echo 'ff    ALL=NOPASSWD:ALL'
+sudo update-alternatives --config editor
+sudo visudo
 
