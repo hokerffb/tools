2ab01bbd0a12086facc7548d121617c2a4524f71
0xff <pengyuwei@gmail.com>
2023-04-25 15:55:17 +0800

    update

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,10 +4,11 @@ wget http://www.memcd.com/conf/ubuntu22.04/source.list.ustc
 sudo cp source.list.ustc /etc/apt/sources.list
 sudo apt update
 
-sudo apt install -y vim build-essential grc git wget net-tools tmux tree
+sudo apt install -y vim grc git wget net-tools tmux tree rsync curl htop
+sudo apt install -y build-essential
 wget http://www.memcd.com/conf/vimrc.local
 sudo cp vimrc.local /etc/vim/
-echo "set -g mouse on" > ~/.tmux.conf 
+echo "set -g mouse on" > ~/.tmux.conf
 
 wget http://www.memcd.com/conf/00-installer-config.yaml
 sudo cp 00-installer-config.yaml /etc/netplan/
