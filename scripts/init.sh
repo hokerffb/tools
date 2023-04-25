c958211f4c6b67c280e46ae9379136726be02d46
0xff <pengyuwei@gmail.com>
2023-04-25 15:32:35 +0800

    update

--- scripts/init.sh
+++ scripts/init.sh
@@ -9,8 +9,8 @@ wget http://www.memcd.com/conf/vimrc.local
 sudo cp vimrc.local /etc/vim/
 echo "set -g mouse on" > ~/.tmux.conf 
 
-wget http://www.memcd.com/conf/docker/daemon.json
-sudo cp daemon.json /etc/docker/
+wget http://www.memcd.com/conf/00-installer-config.yaml
+sudo cp 00-installer-config.yaml /etc/netplan/
 
 wget http://www.memcd.com/conf/.bash_aliases -O ~/.bash_aliases
 source ~/.bash_aliases
@@ -19,3 +19,12 @@ echo 'ff    ALL=NOPASSWD:ALL'
 sudo update-alternatives --config editor
 sudo visudo
 
+sudo vi /etc/hostname
+
+wget http://www.memcd.com/conf/docker/daemon.json
+sudo cp daemon.json /etc/docker/
+sudo groupadd docker
+sudo usermod -a -G docker $USER
+newgrp docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
